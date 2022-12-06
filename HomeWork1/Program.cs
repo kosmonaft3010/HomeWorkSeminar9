714e702e5f32e0765939a8e9746f8d016e606745
kosmonaft3010 <claud055876@gmail.com>
2022-12-06 21:54:05 +0300

    added_home_work_seminar_9

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите число N ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void printAllNaturalNumbers(int m)
+{
+    if (m == 1)
+    {
+        Console.Write(1);
+        return;
+    }
+    else
+    {
+        Console.Write(m + ", ");
+        printAllNaturalNumbers(m - 1);
+    }
+}
+
+printAllNaturalNumbers(n);
---
+++ HomeWork2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+//  которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите начальное число M ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите конечное число N ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PrintSum(m, n));
+
+int PrintSum(int m, int n)
+{
+    int start = m;
+    int end = n;
+    if (m > n)
+    {
+        start = n;
+        end = m;
+    }
+    return (end + start) * (end - start + 1) / 2;
+}
